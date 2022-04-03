a166869200b5ae16749c941cf1c75c2959c4c34b
Matteo Gazzetta <matteo.gazzetta@bitrock.it>
2022-04-03 16:44:35 +0200

    first commit

---
+++ boundary/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    boundary = {
+      source  = "hashicorp/boundary"
+      version = "1.0.5"
+    }
+  }
+}
+
+provider "boundary" {
+  addr             = var.url
+  recovery_kms_hcl = <<EOT
+kms "awskms" {
+	purpose    = "recovery"
+	key_id     = "global_root"
+  kms_key_id = "${var.kms_recovery_key_id}"
+}
+EOT
+}
---
+++ vars.tf
@@ -0,0 +1,11 @@
+variable "boundary_bin" {
+  default = "~/projects/boundary/bin"
+}
+
+variable "pub_ssh_key_path" {
+  default = "~/.ssh/id_rsa.pub"
+}
+
+variable "priv_ssh_key_path" {
+  default = ""
+}
