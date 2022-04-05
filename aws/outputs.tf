8ab9f2ebde3d599dd7ec13c2159cbf8a7180901b
Matteo Gazzetta <matteo.gazzetta@bitrock.it>
2022-04-05 18:45:39 +0200

    Fix output

--- aws/outputs.tf
+++ aws/outputs.tf
@@ -3,16 +3,6 @@ output "dns_name" {
   value       = module.alb.lb_dns_name
 }
 
-output "s3command" {
-  description = "The S3 cp command used to display the contents of the cloud-init-output.log"
-
-  value = format(
-    "aws s3 cp s3://%s/%s -",
-    aws_s3_bucket.boundary.id,
-    data.aws_s3_bucket_objects.cloudinit.keys[0]
-  )
-}
-
 output "kms_recovery_key_id" {
   value = aws_kms_key.recovery.id
 }
