b8f5e18e1296c3807dd73494805ddcc0b89abc7a
Matteo Gazzetta <matteo.gazzetta@bitrock.it>
2022-04-07 12:10:58 +0200

    Add outputs

---
+++ boundary/outputs.tf
@@ -0,0 +1,17 @@
+output "target-ids" {
+  value = [
+    "AWS VM: ${boundary_target.ssh-aws-target.id}",
+    "POSTGRES-DBA: ${boundary_target.psql-dba-target.id}",
+    "POSTGRES: ${boundary_target.psql-target.id}",
+  ]
+}
+
+output "dbadmin-password" {
+  value = [boundary_account.dbadmin.login_name, boundary_account.dbadmin.password]
+}
+
+output "auth-method" {
+  value = [
+    "PASSWORD: ${boundary_auth_method.password.id}"
+  ]
+}
