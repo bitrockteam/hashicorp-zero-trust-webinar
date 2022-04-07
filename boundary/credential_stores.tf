97563d8b912c57b3164b6897efda37e78785fd85
Matteo Gazzetta <matteo.gazzetta@bitrock.it>
2022-04-07 12:48:36 +0200

    Fix roles

--- boundary/credential_stores.tf
+++ boundary/credential_stores.tf
@@ -27,7 +27,7 @@ resource "boundary_credential_library_vault" "psql_dba" {
 
 resource "boundary_credential_library_vault" "psql_analyst" {
   name                = "ERP PSQL Library"
-  description         = "ERP PSQL "
+  description         = "ERP PSQL"
   credential_store_id = boundary_credential_store_vault.vault-erp.id
   path                = var.vault_psql_analyst_path
   http_method         = "GET"
--- boundary/roles.tf
+++ boundary/roles.tf
@@ -24,6 +24,22 @@ resource "boundary_role" "org_anon_listing" {
   principal_ids = ["u_anon"]
 }
 
+# Adds a read-only role in the global scope granting read-only access
+# to all resources within the org scope
+resource "boundary_role" "org_readonly" {
+  name        = "readonly"
+  description = "Read-only role"
+  principal_ids = [
+    boundary_user.operation.id,
+    boundary_user.dbadmin.id
+  ]
+  grant_strings = [
+    "id=*;type=*;actions=read"
+  ]
+  scope_id       = boundary_scope.global.id
+  grant_scope_id = boundary_scope.org.id
+}
+
 
 # Creates a role in the global scope that's granting administrative access to
 # resources in the org scope for admin user
@@ -39,17 +55,42 @@ resource "boundary_role" "org_admin" {
 }
 
 
-// Organization
+# Adds an org-level role granting administrative permissions within the projects
+resource "boundary_role" "project_erp_admin" {
+  name           = "project_erp_admin"
+  description    = "Administrator role for northwind erp"
+  scope_id       = boundary_scope.org.id
+  grant_scope_id = boundary_scope.project-northwind-erp.id
+  grant_strings = [
+    "id=*;type=*;actions=*"
+  ]
+  principal_ids = [
+    boundary_user.admin.id
+  ]
+}
+
+resource "boundary_role" "project_prod_support" {
+  name           = "project_prod_support"
+  description    = "Administrator role for prod support"
+  scope_id       = boundary_scope.org.id
+  grant_scope_id = boundary_scope.project-prod-support.id
+  grant_strings = [
+    "id=*;type=*;actions=*"
+  ]
+  principal_ids = [
+    boundary_user.admin.id
+  ]
+}
 
 resource "boundary_role" "server-admin" {
   name           = "Server Admin Role"
+  scope_id       = boundary_scope.org.id
   grant_scope_id = boundary_scope.project-prod-support.id
   grant_strings = [
-    "id=${boundary_target.ssh-aws-target.id};actions=*",
+    "id=${boundary_target.ssh-aws-target.id};type=*;actions=*",
     "id=*;type=session;actions=cancel:self,read",
     "id=*;type=*;actions=read,list"
   ]
-  scope_id      = boundary_scope.org.id
   principal_ids = [boundary_user.operation.id]
 }
 
@@ -57,12 +98,12 @@ resource "boundary_role" "server-admin" {
 //
 resource "boundary_role" "psql-admin" {
   name           = "PSQL Admin Role"
+  scope_id       = boundary_scope.org.id
   grant_scope_id = boundary_scope.project-prod-support.id
   grant_strings = [
-    "id=${boundary_target.psql-target.id};actions=*",
+    "id=${boundary_target.psql-dba-target.id};actions=*",
     "id=*;type=session;actions=cancel:self,read",
     "id=*;type=*;actions=read,list"
   ]
-  scope_id      = boundary_scope.org.id
   principal_ids = [boundary_user.dbadmin.id]
 }
