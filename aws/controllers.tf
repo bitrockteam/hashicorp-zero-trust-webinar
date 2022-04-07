fe34bb3db277deb900b855eb61e1a4a3812e2b12
Matteo Gazzetta <matteo.gazzetta@bitrock.it>
2022-04-07 14:37:31 +0200

    Allow bastion

--- aws/controllers.tf
+++ aws/controllers.tf
@@ -115,7 +115,7 @@ resource "aws_security_group" "postgresql" {
   ingress {
     from_port       = 5432
     protocol        = "TCP"
-    security_groups = [aws_security_group.controller.id, aws_security_group.internal_vault.id, aws_security_group.worker.id]
+    security_groups = [aws_security_group.controller.id, aws_security_group.internal_vault.id, aws_security_group.worker.id, aws_security_group.bastion.id]
     to_port         = 5432
   }
 
